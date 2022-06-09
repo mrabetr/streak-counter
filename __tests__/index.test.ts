808db8f254f903ffcc219dd6268ddb3d06a2631e
rafik mrabet <mrabetr@hotmail.com>
2022-06-09 14:25:03 +0100

    added nested tested for pre-populated streak

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,6 @@
 import { JSDOM } from "jsdom";
 import { streakCounter } from "../src/index";
+import { formattedDate } from "../src/utils";
 
 describe('streakCounter', () => {
   let mockLocalStorage: Storage;
@@ -28,13 +29,6 @@ describe('streakCounter', () => {
     const date = new Date()
     const streak = streakCounter(mockLocalStorage, date)
 
-    function formattedDate(date: Date): string {
-      // returns date as 11/11/2021
-      // other times it returns 11/11/2021, 12:00:00 AM
-      // which is why we call the .split at the end
-      return date.toLocaleDateString('en-US')
-    }
-
     const dateFormatted = formattedDate(date)
 
     expect(streak.currentCount).toBe(1)
@@ -48,5 +42,38 @@ describe('streakCounter', () => {
 
     const streakAsString = mockLocalStorage.getItem(key)
     expect(streakAsString).not.toBeNull()
+  })
+
+  describe('with a pre-populated streak', () => {
+    let mockLocalStorage: Storage
+
+    beforeEach(() => {
+      const mockJSDom = new JSDOM('', {url: 'https://localhost'})
+
+      mockLocalStorage = mockJSDom.window.localStorage
+
+      // Use date in past so it’s always the same
+      const date = new Date('12/12/2021')
+
+      const streak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date),
+      }
+
+      mockLocalStorage.setItem('streak', JSON.stringify(streak))
+    });
+
+    afterEach(() => {
+      mockLocalStorage.clear()
+    });
+
+    it('should return the streak from localStorage', () => {
+      const date = new Date()
+      const streak = streakCounter(mockLocalStorage, date)
+
+      // Should match the dates used to set up the tests
+      expect(streak.startDate).toBe('12/12/2021')
+    })
   })
 })
