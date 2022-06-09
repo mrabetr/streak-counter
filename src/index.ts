f21c621935eb92df72ce05bf145ea8c95fb69cf0
rafik mrabet <mrabetr@hotmail.com>
2022-06-09 15:14:39 +0100

    Write tests for initializeStreak updates

--- src/index.ts
+++ src/index.ts
@@ -15,18 +15,22 @@ const KEY = 'streak'
 function shouldIncrementOrResetStreakCount(
   currentDate: Date,
   lastLoginDate: string,
-): 'increment' | undefined {
+): 'increment' | 'reset' | 'none' {
   // We get 11/5/2021
   // so to get 5, we split on / and get the second item
   const difference =
     currentDate.getDate() - parseInt(lastLoginDate.split('/')[1])
+  // Same-day login, do nothing
+  if (difference === 0) {
+    return "none";
+  }
   // This means they logged in the day after the currentDate
   if (difference === 1) {
     return 'increment'
   }
   // Otherwise they logged in after a day, which would
   // break the streak
-  return undefined
+  return 'reset'
 }
 
 export function streakCounter(storage: Storage, date: Date): Streak {
@@ -38,11 +42,23 @@ export function streakCounter(storage: Storage, date: Date): Streak {
       // const state = "increment";
       const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate)
       const SHOULD_INCREMENT = state === "increment";
+      const SHOULD_RESET = state === "reset";
 
       if (SHOULD_INCREMENT) {
         const updatedStreak = {
           ...streak,
           currentCount: streak.currentCount + 1,
+          lastLoginDate: formattedDate(date),
+        };
+        // store in localStorage
+        storage.setItem(KEY, JSON.stringify(updatedStreak));
+
+        return updatedStreak;
+      }
+      if (SHOULD_RESET) {
+        const updatedStreak: Streak = {
+          currentCount: 1,
+          startDate: formattedDate(date),
           lastLoginDate: formattedDate(date),
         };
         // store in localStorage
